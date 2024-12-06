9054333ade166b2d8f2156ae104b0e4cd9a7b3f8
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-06 20:39:37 +0530

    Expressions Code Formatting

--- compiler/codegenerator.py
+++ compiler/codegenerator.py
@@ -19,13 +19,15 @@ class CodeGenerator(Transformer):
         #self.inFunctionScope = False
         #self.functionScope = [{}]
 
+    ## TODO: separate this class into separate files for different categories of functions
+    ## TODO: replace self.output.append with suitable code output functions
 
     def indent(self):
         self.indentLevel += 1
     def unindent(self):
         self.indentLevel -= 1
 
-    def code(self, instruction: str):
+    def code(self, instruction: str, newLine1 = False, newLine2 = False):
         indent = '\t' * self.indentLevel
         label = ''
 
@@ -33,14 +35,22 @@ class CodeGenerator(Transformer):
 
         if words[0].startswith('@'):
             label = words.pop(0)
+            label += ' '
+
+        newLineChar1 = '\n' if newLine1 else ''
+        newLineChar2 = '\n' if newLine2 else ''
 
         formatted_words = ' '.join([f"{word:<5}" for word in words[1:]])
-        self.output.append(f"{indent}{label} {words[0]:<7} {formatted_words}")
+        self.output.append(f"{newLineChar1}{indent}{label}{words[0]:<7} {formatted_words}{newLineChar2}")
         self.instructions =+ 1
 
-    def comment(self, comment: str):
+    def comment(self, comment: str, newLine1 = False, newLine2 = False):
         indent = '\t' * self.indentLevel
-        self.output.append(f"{indent}{comment}")
+        
+        newLineChar1 = '\n' if newLine1 else ''
+        newLineChar2 = '\n' if newLine2 else ''
+
+        self.output.append(f"{newLineChar1}{indent}{comment}{newLineChar2}")
 
 
     ## VARIABLES ##
@@ -145,26 +155,45 @@ class CodeGenerator(Transformer):
         resultRegister = self.exprRegisters.pop()
         expressionBasePointer = self.stackPointer
 
-        self.output.append(f"\n# EXPRESSION START #")
+        symbols = {
+            "multiply":     "*",  "divide":                "/", "modulus": "%",
+            "add":          "+",  "subtract":              "-",
+            "equal":        "==", "not_equal":             "!=",
+            "lesser_than":  "<",  "lesser_than_or_equal":  "<=",
+            "greater_than": ">",  "greater_than_or_equal": ">=",
+            "logical_and":  "&&", "logical_or":            "||",
+            "left_parenthesis": "(", "right_parenthesis": ")"
+        }
+
+        infixExpr = ''
+
+        for exprToken in node:
+            if isinstance(exprToken, tuple):
+                infixExpr += str(exprToken[1]) + ' '
+            else:
+                infixExpr += symbols[str(exprToken.data)] + ' '
+
+        self.comment(f"# EXPRESSION [ {infixExpr}]: ")
+        self.indent()
 
         if len(postfix_expr) == 1:
             if postfix_expr[0][0] == "num":
-                self.output.append(f"\n# {resultRegister} = {int(postfix_expr[0][1])}")
-                self.output.append(f"IADDI 0 {int(postfix_expr[0][1])} {resultRegister}")
+                self.comment(f"# {resultRegister} = {int(postfix_expr[0][1])}")
+                self.code(f"IADDI 0 {int(postfix_expr[0][1])} {resultRegister}")
             elif postfix_expr[0][0] == "var":
                 var_name = str(postfix_expr[0][1])
                 var_address = self.get_variable_address(var_name)
                 
-                self.output.append(f"\n# {resultRegister} = {var_name}")
-                self.output.append(f"ILOADI {var_address} 0 {resultRegister}")
+                self.comment(f"# {resultRegister} = {var_name}")
+                self.code(f"ILOADI {var_address} 0 {resultRegister}")
         else:
             for token in postfix_expr:
                 tokenType, tokenValue = token
 
                 if tokenType == "num":
                     self.stackPointer -= 1
-                    self.output.append(f"\n# [{self.stackPointer}] = {tokenValue}")
-                    self.output.append(f"ISTOREI {self.stackPointer} {int(tokenValue)} 0")
+                    self.comment(f"# [{self.stackPointer}] = {tokenValue}")
+                    self.code(f"ISTOREI {self.stackPointer} {int(tokenValue)} 0", newLine2=True)
                 elif tokenType == "var":
                     self.stackPointer -= 1
                     tempRegister = self.exprRegisters.pop()
@@ -172,9 +201,9 @@ class CodeGenerator(Transformer):
                     var_name = str(tokenValue)
                     var_address = self.get_variable_address(var_name)
 
-                    self.output.append(f"\n# [{self.stackPointer}] = {var_name}")
-                    self.output.append(f"ILOADI {int(var_address)} 0 {tempRegister}")
-                    self.output.append(f"ISTORE {self.stackPointer} {tempRegister} 0")
+                    self.comment(f"# [{self.stackPointer}] = {var_name}")
+                    self.code(f"ILOADI {int(var_address)} 0 {tempRegister}")
+                    self.code(f"ISTORE {self.stackPointer} {tempRegister} 0", newLine2=True)
 
                     self.exprRegisters.append(tempRegister)
                 elif tokenType == "operator":
@@ -193,22 +222,24 @@ class CodeGenerator(Transformer):
 
                     instruction = operations[tokenValue]
 
-                    self.output.append(f"\n# [{self.stackPointer}] = [{self.stackPointer + 1}] {tokenValue} [{self.stackPointer}]")
-                    self.output.append(f"ILOADI {self.stackPointer} 0 {register2}")
+                    self.comment(f"# [{self.stackPointer}] = [{self.stackPointer + 1}] {tokenValue} [{self.stackPointer}]")
+                    self.code(f"ILOADI {self.stackPointer} 0 {register2}")
                     self.stackPointer += 1
-                    self.output.append(f"ILOADI {self.stackPointer} 0 {register1}")
+                    self.code(f"ILOADI {self.stackPointer} 0 {register1}")
                     
-                    self.output.append(f"{instruction} {register1} {register2} {tempRegister}")
-                    self.output.append(f"ISTORE {self.stackPointer} {tempRegister} 0")
+                    self.code(f"{instruction} {register1} {register2} {tempRegister}")
+                    self.code(f"ISTORE {self.stackPointer} {tempRegister} 0", newLine2=True)
 
                     self.exprRegisters.append(tempRegister)
                     self.exprRegisters.append(register2)
                     self.exprRegisters.append(register1)
 
-            self.output.append(f"\n# {resultRegister} = [{self.stackPointer}]")
-            self.output.append(f"ILOADI {self.stackPointer} 0 {resultRegister}")
+            self.comment(f"# {resultRegister} = [{self.stackPointer}]")
+            self.code(f"ILOADI {self.stackPointer} 0 {resultRegister}")
         
-        self.output.append(f"\n# EXPRESSION END #")
+        self.unindent()
+        self.comment(f"# END EXPRESSION [{resultRegister}]", newLine2=True)
+
         self.stackPointer = expressionBasePointer
         return ("register", str(resultRegister))
 
@@ -225,8 +256,8 @@ class CodeGenerator(Transformer):
                 self.stackPointer -= 1
                 self.declare_variable(var_name, self.stackPointer)
 
-                self.output.append(f"\n# {var_type} {var_name} = {register}")
-                self.output.append(f"ISTORE {self.stackPointer} {register} 0")
+                self.comment(f"# {var_type} {var_name} = {register}")
+                self.code(f"ISTORE {self.stackPointer} {register} 0")
                 self.exprRegisters.append(register)
             else:
                 raise MemoryError("ERROR: expression register not found for var_decl")
@@ -234,8 +265,8 @@ class CodeGenerator(Transformer):
             self.stackPointer -= 1
             self.declare_variable(var_name, self.stackPointer)
 
-            self.output.append(f"\n# {var_type} {var_name} = 0")
-            self.output.append(f"ISTOREI {self.stackPointer} 0 0")
+            self.comment(f"# {var_type} {var_name} = 0")
+            self.code(f"ISTOREI {self.stackPointer} 0 0")
     
     def assign_stmt(self, node):
         var_name = str(node[0])
@@ -244,8 +275,8 @@ class CodeGenerator(Transformer):
             register = node[1][1]
             var_address = self.get_variable_address(var_name)
 
-            self.output.append(f"\n# {var_name} = {register}")
-            self.output.append(f"ISTORE {var_address} {register} 0")
+            self.comment(f"# {var_name} = {register}")
+            self.code(f"ISTORE {var_address} {register} 0")
             self.exprRegisters.append(register)
         else:
             raise MemoryError("ERROR: expression register not found for var_decl")
@@ -256,8 +287,8 @@ class CodeGenerator(Transformer):
         if (isinstance(node[0], tuple)) and (node[0][0] == "register"):
             register = node[0][1]
 
-            self.output.append(f"\n## IF START ##")
-            self.output.append(f"LNOTI {register} 0 {register}")
+            self.comment(f"## IF START ##")
+            self.code(f"LNOTI {register} 0 {register}")
 
             # push 1 label for end of if block which can be continued by else if, else statements
             # and push 1 label for end of entire if ladder
@@ -266,7 +297,7 @@ class CodeGenerator(Transformer):
             self.highestLabel += 1
             self.labels.append(f"ifLabel{self.highestLabel}")
 
-            self.output.append(f"JMPI {register} 0 @{self.labels[-1]}")
+            self.code(f"JMPI {register} 0 @{self.labels[-1]}")
             self.exprRegisters.append(register)
         else:
             raise MemoryError("ERROR: expression register not found for if_condition")
@@ -275,14 +306,14 @@ class CodeGenerator(Transformer):
         if (isinstance(node[0], tuple)) and (node[0][0] == "register"):
             register = node[0][1]
 
-            self.output.append(f"\n## ELSE IF START ##")
-            self.output.append(f"LNOTI {register} 0 {register}")
+            self.comment(f"## ELSE IF START ##")
+            self.code(f"LNOTI {register} 0 {register}")
 
             # push 1 label for end of else if block
             self.highestLabel += 1
             self.labels.append(f"elseIfLabel{self.highestLabel}")
 
-            self.output.append(f"JMPI {register} 0 @{self.labels[-1]}")
+            self.code(f"JMPI {register} 0 @{self.labels[-1]}")
             self.exprRegisters.append(register)
         else:
             raise MemoryError("ERROR: expression register not found for else_if_condition")
@@ -293,18 +324,18 @@ class CodeGenerator(Transformer):
     def if_end(self, node):
         label = self.labels.pop()
 
-        self.output.append(f"\n## IF END ##")
-        self.output.append(f"IJMPI 1 0 @{self.labels[-1]}")
-        self.output.append(f"@{label}: IADDI 0 0 0") # dummy instruction due to how labels are assembled
+        self.comment(f"## IF END ##")
+        self.code(f"IJMPI 1 0 @{self.labels[-1]}")
+        self.code(f"@{label}: IADDI 0 0 0") # dummy instruction due to how labels are assembled
         self.exit_scope()
 
     def else_end(self, node):
-        self.output.append(f"\n## ELSE END ##")
+        self.comment(f"## ELSE END ##")
         self.exit_scope()
 
     def if_stmt(self, node):
         label = self.labels.pop()
-        self.output.append(f"\n@{label}: IADDI 0 0 0") # dummy instruction due to how labels are assembled
+        self.code(f"\n@{label}: IADDI 0 0 0") # dummy instruction due to how labels are assembled
 
 
     ## WHILE LOOP ##
@@ -317,15 +348,15 @@ class CodeGenerator(Transformer):
         self.highestLabel += 1
         self.labels.append(f"whileLabel{self.highestLabel}")
 
-        self.output.append(f"\n## WHILE LOOP START ##")
-        self.output.append(f"@{self.labels[-2]}: IADDI 0 0 0")
+        self.comment(f"## WHILE LOOP START ##")
+        self.code(f"@{self.labels[-2]}: IADDI 0 0 0")
 
     def while_condition(self, node):
         if (isinstance(node[0], tuple)) and (node[0][0] == "register"):
             register = node[0][1]
 
-            self.output.append(f"\nLNOTI {register} 0 {register}")
-            self.output.append(f"JMPI {register} 0 @{self.labels[-1]}")
+            self.code(f"\nLNOTI {register} 0 {register}")
+            self.code(f"JMPI {register} 0 @{self.labels[-1]}")
             self.exprRegisters.append(register)
 
             self.enter_scope()
@@ -333,9 +364,9 @@ class CodeGenerator(Transformer):
             raise MemoryError("ERROR: expression register not found for while_condition")
     
     def while_stmt(self, node):
-        self.output.append(f"\nIJMPI 1 0 @{self.labels[-2]}") # always go back to start of loop
-        self.output.append(f"@{self.labels[-1]}: IADDI 0 0 0") # end of loop in case condition false
-        self.output.append(f"\n## WHILE LOOP END ##")
+        self.code(f"\nIJMPI 1 0 @{self.labels[-2]}") # always go back to start of loop
+        self.code(f"@{self.labels[-1]}: IADDI 0 0 0") # end of loop in case condition false
+        self.comment(f"## WHILE LOOP END ##")
 
         self.exit_scope()
         self.labels.pop()
@@ -357,16 +388,16 @@ class CodeGenerator(Transformer):
         self.highestLabel += 1
         self.labels.append(f"forLabel{self.highestLabel}")
 
-        self.output.append(f"\n## FOR LOOP START ##")
-        self.output.append(f"@{self.labels[-4]}: IADDI 0 0 0")
+        self.comment(f"## FOR LOOP START ##")
+        self.code(f"@{self.labels[-4]}: IADDI 0 0 0")
 
     def for_condition(self, node):
         if (isinstance(node[0], tuple)) and (node[0][0] == "register"):
             register = node[0][1]
 
-            self.output.append(f"\n# FOR LOOP CONDITION #")
-            self.output.append(f"LNOTI {register} 0 {register}")
-            self.output.append(f"JMPI {register} 0 @{self.labels[-1]}")
+            self.comment(f"# FOR LOOP CONDITION #")
+            self.code(f"LNOTI {register} 0 {register}")
+            self.code(f"JMPI {register} 0 @{self.labels[-1]}")
             self.exprRegisters.append(register)
         else:
             raise MemoryError("ERROR: expression register not found for for_condition")
@@ -374,17 +405,17 @@ class CodeGenerator(Transformer):
     def for_update_start(self, node):
         # skip evaluating assign stmt first, then come back to it after loop block is evaluated
         # at end of assign stmt evaluation, jmp back to the condition of loop
-        self.output.append(f"\nIJMPI 1 0 @{self.labels[-3]}")
-        self.output.append(f"@{self.labels[-2]}: IADDI 0 0 0")
+        self.code(f"\nIJMPI 1 0 @{self.labels[-3]}")
+        self.code(f"@{self.labels[-2]}: IADDI 0 0 0")
 
     def for_update_end(self, node):
-        self.output.append(f"\nIJMPI 1 0 @{self.labels[-4]}")
-        self.output.append(f"@{self.labels[-3]}: IADDI 0 0 0")
+        self.code(f"\nIJMPI 1 0 @{self.labels[-4]}")
+        self.code(f"@{self.labels[-3]}: IADDI 0 0 0")
 
     def for_stmt(self, node):
-        self.output.append(f"\nIJMPI 1 0 @{self.labels[-2]}")
-        self.output.append(f"@{self.labels[-1]}: IADDI 0 0 0")
-        self.output.append(f"\n## FOR END ##")
+        self.code(f"\nIJMPI 1 0 @{self.labels[-2]}")
+        self.code(f"@{self.labels[-1]}: IADDI 0 0 0")
+        self.comment(f"## FOR END ##")
 
         self.exit_scope()
         self.labels.pop()
@@ -399,8 +430,8 @@ class CodeGenerator(Transformer):
             register1 = node[0][1]
             register2 = node[1][1]
 
-            self.output.append(f"\n# PUT PIXEL")
-            self.output.append(f"PIXEL {register1} {register2} 0")
+            self.comment(f"# PUT PIXEL")
+            self.code(f"PIXEL {register1} {register2} 0")
 
             self.exprRegisters.append(register2)
             self.exprRegisters.append(register1)
@@ -413,10 +444,10 @@ class CodeGenerator(Transformer):
             register2 = node[1][1]
             register3 = node[2][1]
 
-            self.output.append(f"\n# COLOR")
-            self.output.append(f"SHLI {register1} 8 {register1}")
-            self.output.append(f"ADD {register1} {register2} {register1}")
-            self.output.append(f"COLOR {register1} {register3} 0")
+            self.comment(f"# COLOR")
+            self.code(f"SHLI {register1} 8 {register1}")
+            self.code(f"ADD {register1} {register2} {register1}")
+            self.code(f"COLOR {register1} {register3} 0")
             
             self.exprRegisters.append(register3)
             self.exprRegisters.append(register2)
@@ -425,22 +456,22 @@ class CodeGenerator(Transformer):
             raise MemoryError("ERROR: expression register not found for color")
         
     def fill(self, node):
-        self.output.append(f"\n# FILL")
-        self.output.append(f"IFILLI 0 0 0")
+        self.comment(f"# FILL")
+        self.code(f"IFILLI 0 0 0")
 
 
     ## UTILITY FUNCTIONS ##
     def exit(self, node):
-        self.output.append(f"\n# EXIT")
-        self.output.append(f"HALT 0 0 0")
+        self.comment(f"# EXIT")
+        self.code(f"HALT 0 0 0")
 
 
     def generate_code(self):
         # DEBUG: Requires result variable to be declared in code | Displays value of result on R15
         var_address = self.get_variable_address("result")
 
-        self.output.append(f"\n# DEBUG: Load variable result into R15")
-        self.output.append(f"ILOADI {var_address} 0 R15")
+        self.comment(f"# DEBUG: Load variable result into R15")
+        self.code(f"ILOADI {var_address} 0 R15")
 
-        self.output.append("\nHALT 0 0 0\n") # End of program
+        self.code("\nHALT 0 0 0\n") # End of program
         return "\n".join(self.output)
