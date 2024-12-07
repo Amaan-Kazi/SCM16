d2a54102f603cf1a32a68e6a4ca235f207379e09
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-07 21:14:51 +0530

    Display and Utility Functions Code Formatting

--- compiler/codegenerator.py
+++ compiler/codegenerator.py
@@ -459,29 +459,45 @@ class CodeGenerator(Transformer):
 
     
     ## DOT MATRIX DISPLAY FUNCTIONS ##
+    def putpixel_start(self, node):
+        self.comment(f"# PUTPIXEL(): ")
+        self.indent()
+    
     def putpixel(self, node):
+        node.pop(0) # removal of unneeded empty node from putpixel_start
         if ((isinstance(node[0], tuple)) and (node[0][0] == "register") and (isinstance(node[1], tuple)) and (node[1][0] == "register")):
             register1 = node[0][1]
             register2 = node[1][1]
 
-            self.comment(f"# PUT PIXEL")
+            self.comment(f"# PUT PIXEL [ {register1}, {register2} ]")
             self.code(f"PIXEL {register1} {register2} 0")
+
+            self.unindent()
+            self.comment(f"# END PUTPIXEL()", newLine2=True)
 
             self.exprRegisters.append(register2)
             self.exprRegisters.append(register1)
         else:
             raise MemoryError("ERROR: expression register not found for putpixel")
 
+    def color_start(self, node):
+        self.comment(f"# COLOR(): ")
+        self.indent()
+
     def color(self, node):
+        node.pop(0) # removal of unneeded empty node from color_start
         if ((isinstance(node[0], tuple)) and (node[0][0] == "register") and (isinstance(node[1], tuple)) and (node[1][0] == "register") and (isinstance(node[2], tuple)) and (node[2][0] == "register")):
             register1 = node[0][1]
             register2 = node[1][1]
             register3 = node[2][1]
 
-            self.comment(f"# COLOR")
+            self.comment(f"# COLOR [ {register1}, {register2}, {register3} ]")
             self.code(f"SHLI {register1} 8 {register1}")
             self.code(f"ADD {register1} {register2} {register1}")
             self.code(f"COLOR {register1} {register3} 0")
+
+            self.unindent()
+            self.comment(f"# END COLOR()", newLine2=True)
             
             self.exprRegisters.append(register3)
             self.exprRegisters.append(register2)
@@ -490,14 +506,14 @@ class CodeGenerator(Transformer):
             raise MemoryError("ERROR: expression register not found for color")
         
     def fill(self, node):
-        self.comment(f"# FILL")
-        self.code(f"IFILLI 0 0 0")
+        self.comment(f"# FILL(): ")
+        self.code(f"IFILLI 0 0 0", newLine2=True)
 
 
     ## UTILITY FUNCTIONS ##
     def exit(self, node):
-        self.comment(f"# EXIT")
-        self.code(f"HALT 0 0 0")
+        self.comment(f"# EXIT(): ")
+        self.code(f"HALT 0 0 0", newLine2=True)
 
 
     def generate_code(self):
@@ -507,5 +523,5 @@ class CodeGenerator(Transformer):
         self.comment(f"# DEBUG: Load variable result into R15")
         self.code(f"ILOADI {var_address} 0 R15")
 
-        self.code("\nHALT 0 0 0\n") # End of program
+        self.code("HALT 0 0 0", newLine2=True) # End of program
         return "\n".join(self.output)
